c05eea0d4f5def94f5425e96fd195ab18c42f011
imranhassanaiub <imranhassanaiub@gmail.com>
2026-07-03 20:59:22 +0600

    Fixed some flows

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -3,9 +3,8 @@ package Runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "/Users/md.imranhassan/eclipse-workspace-newversion/qaproject/src/test/resources/features", glue = {
-		"Stepdefinitions" }, plugin = { "pretty",
-				"html:target/cucumber-reports.html" }, monochrome = true, tags = "@e2epurchase")
+@CucumberOptions(features = "src/test/resources/features", glue = { "Stepdefinitions" }, plugin = { "pretty",
+		"html:target/cucumber-reports.html" }, monochrome = true, tags = "@e2e or @navigation or @negative")
 
 public class Runner extends AbstractTestNGCucumberTests {
 
