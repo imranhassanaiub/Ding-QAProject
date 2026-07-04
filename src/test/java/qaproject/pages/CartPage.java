586a5a4c54bbd62d1d95007f2b57f8c6ca806b6c
imranhassanaiub <imranhassanaiub@gmail.com>
2026-07-04 12:46:33 +0600

    updated some classes

--- src/test/java/qaproject/pages/CartPage.java
+++ src/test/java/qaproject/pages/CartPage.java
@@ -4,11 +4,11 @@ public class CartPage {
 
 	public static class CartModules{
 		
-		public static final String checkoutbtn = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[2]";
+		public static final String checkoutbtn = "//button[@id='checkout']";
 		public static final String continueShopping = "//button[@id='continue-shopping']";
-		public static final String checkoutFname = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[1]/input[1]";
-		public static final String checkoutLname = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[2]/input[1]";
-		public static final String checkoutPostalCode = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[1]/div[3]/input[1]";
+		public static final String checkoutFname = "//input[@id='first-name']";
+		public static final String checkoutLname = "//input[@id='last-name']";
+		public static final String checkoutPostalCode = "//input[@id='postal-code']";
 		public static final String checkoutContinueBtn = "//input[@id='continue']";
 		public static final String checkoutCancelBtn = "//button[@id='cancel']";
         public static final String Finalbtn = "//button[@id='finish']";
