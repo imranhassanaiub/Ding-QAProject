8d3166d331b83b3d064c86deaa4867c43a85de55
imranhassanaiub <imranhassanaiub@gmail.com>
2026-07-04 12:49:54 +0600

    updated some functions and classes

--- src/test/java/qaproject/pages/ItemsSelectionPage.java
+++ src/test/java/qaproject/pages/ItemsSelectionPage.java
@@ -6,7 +6,7 @@ public class ItemsSelectionPage {
 	
 		public static final String item1 = "add-to-cart-sauce-labs-backpack";
 		public static final String item2 = "add-to-cart-sauce-labs-bike-light";
-		public static final String cart = "/html[1]/body[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/a[1]/span[1]";
+		public static final String cart = "//a[@class='shopping_cart_link']";
 		public static final String item1present = "//div[normalize-space()='Sauce Labs Backpack']";
 		public static final String item2present = "//div[normalize-space()='Sauce Labs Bike Light']";
 	}
